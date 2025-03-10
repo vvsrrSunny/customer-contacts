925c06f480fb9b50afa62c1c27d5d5be491e5adb
Sunny <sunny@Sunnys-MacBook-Pro.local>
2025-03-11 06:07:57 +1100

    added migration files

---
+++ database/migrations/2025_03_10_190534_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('reference');
+            $table->string('category');
+            $table->date('start_date');
+            $table->text('description')->nullable();
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
